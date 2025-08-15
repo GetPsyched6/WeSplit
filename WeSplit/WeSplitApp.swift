813270f18974becc9ea328ecd3b352d01788d2bf
Roshin Nishad <roshin006@gmail.com>
2025-08-15 23:49:24 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Roshin Nishad on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
