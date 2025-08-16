0f73ed450d0ee8674b5c1b9580ce062ff529f6bc
Roshin Nishad <roshin006@gmail.com>
2025-08-16 19:38:20 +0530

    Created Bill Splitting app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @FocusState private var amountIsFocused: Bool
+  @State private var checkAmount = 0.0
+  @State private var noOfPeople = 2
+  @State private var tipPercentage = 20
+  let tipPercentages = [10, 15, 20, 25, 0]
+  var totals: Double {
+    ((Double(tipPercentage) * checkAmount) / 100) + checkAmount
+  }
+
+  var totalPerPerson: Double {
+    totals
+      / Double(noOfPeople + 2)
+  }
+
+  var body: some View {
+    NavigationStack {
+      Form {
+        Section {
+          LabeledContent {
+            TextField(
+              "Amount",
+              value: $checkAmount,
+              format: .currency(
+                code: Locale.current.currency?.identifier
+                  ?? "USD"
+              )
+            ).focused($amountIsFocused)
+              .keyboardType(.decimalPad)
+          } label: {
+            Text("Check Amount")
+          }
+          Picker("Number of People", selection: $noOfPeople) {
+            ForEach(2..<200) {
+              Text("\($0) people")
+            }
+          }
         }
-        .padding()
+        Section("Tip Percentage") {
+          Picker("Tip Percentage", selection: $tipPercentage) {
+            ForEach(tipPercentages, id: \.self) {
+              Text($0, format: .percent)
+            }
+          }.pickerStyle(.segmented)
+        }
+        Section("Result") {
+          LabeledContent {
+            Text(
+              totals,
+              format: .currency(
+                code: Locale.current.currency?.identifier
+                  ?? "USD"
+              )
+            )
+          } label: {
+            Text("Total")
+          }
+          LabeledContent {
+            Text(
+              totalPerPerson,
+              format: .currency(
+                code: Locale.current.currency?.identifier
+                  ?? "USD"
+              )
+            )
+          } label: {
+            Text("Total Per Person")
+          }
+        }
+      }.navigationTitle("WeSplit").toolbar {
+        if amountIsFocused {
+          Button("Done") {
+            amountIsFocused = false
+          }
+        }
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
